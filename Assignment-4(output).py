99e8eac4ce7bdf7e72808a6a55ca3c457ffaceb4
moksh21102014 <104289232+moksh21102014@users.noreply.github.com>
2022-06-12 22:00:57 +0530

    Create Assignment-4(output).py

---
+++ Assignment-4(output).py
@@ -0,0 +1,35 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/laptop/AppData/Local/Programs/Python/Python310/a4.1.1.py ==
+Enter Marks :60
+ Grade B 
+
+== RESTART: C:/Users/laptop/AppData/Local/Programs/Python/Python310/a4.1.1.py ==
+Enter a year: 2000
+year is a Leap Year
+
+== RESTART: C:/Users/laptop/AppData/Local/Programs/Python/Python310/a4.1.1.py ==
+Question 1:5X10=50
+Right!
+Question 2:10X5=60
+Wrong.The right answer is 50
+Question 3:7X9=63
+Right!
+Question 4:9X9=81
+Right!
+Question 5:7X6=41
+Wrong.The right answer is 42
+Question 6:10X6=60
+Right!
+Question 7:1X6=6
+Right!
+Question 8:5X9=50
+Wrong.The right answer is 45
+Question 9:4X3=16
+Wrong.The right answer is 12
+Question 10:10X1=10
+Right!
+
+== RESTART: C:/Users/laptop/AppData/Local/Programs/Python/Python310/a4.1.1.py ==
+177 candies are in the bowl!
